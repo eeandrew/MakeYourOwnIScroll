0a5159b85731bc16623ee07f328597e560d31b07
zhou lin <boyzhoulin@sina.com>
2016-01-18 14:34:50 +0800

    边缘检测。当Y轴超出边界时，增加收回动画。

--- src/iscroll.js
+++ src/iscroll.js
@@ -5,7 +5,11 @@
 	 	//拿到里层的scroller
 	 	this.scroller = this.wrapper.children[0];
 	 	//拿到里层scroller的style
-		this.scrollerStyle = this.scroller.style;	 	
+		this.scrollerStyle = this.scroller.style;	
+		//配置
+		this.options = {
+			bounce : true,
+		}; 	
 	 	//加入监听器
 	 	this._initEvents();
 	 	//初始值
@@ -41,6 +45,7 @@
 	 		utils.addEvent(window,'mousemove',this);
 	 		utils.addEvent(window,'mousecancel',this);
 	 		utils.addEvent(window,'mouseup',this);
+	 		utils.addEvent(this.scroller,'transitionend',this);
 	 	},
 	 	handleEvent : function(e) {
 	 		switch(e.type) {
@@ -54,6 +59,9 @@
 	 			case 'mouseup':
 	 				this._end(e);
 	 				break;
+	 			case 'transitionend':
+	 				this.scrollerStyle.transitionDuration = 0 + 'ms';
+	 				break;
 	 		}
 	 	},
 	 	_start : function(e) {
@@ -65,6 +73,9 @@
 	 		
 	 		this.pointX    = point.pageX;
 			this.pointY    = point.pageY;
+
+			this.startX = this.x;
+			this.startY = this.y;
 	 	},
 	 	_move : function(e) {
 	 		if(!this.enabled) return;
@@ -79,9 +90,10 @@
 	 		newX = this.x + deltaX;
 	 		newY = this.y + deltaY;
 
-	 		//边缘检测
-	 		if(newY > 0) newY = 0;
-	 		if(newY < this.maxScrollY) newY = this.maxScrollY;
+	 		//处于可滑动边缘以外时，减速
+	 		if(newY > 0 || newY < this.maxScrollY){
+	 			newY = this.options.bounce ? this.y + deltaY /3 : newY > 0 ? 0 :this.maxScrollY;
+	 		}
 	 		this._translate(0,newY);
 	 	},
 	 	_translate : function(x,y) {
@@ -89,8 +101,25 @@
 	 		this.x = x;
 	 		this.y = y;
 	 	},
+	 	scrollTo : function(x,y,time,easing) {
+	 		this.scrollerStyle.transitionTimingFunction = easing.style;
+	 		this.scrollerStyle.transitionDuration = time + 'ms';
+	 		this._translate(x,y);
+	 	}, 
 	 	_end : function(e) {
+	 		var point = e,
+	 		newX = Math.round(this.x),
+	 		newY = Math.round(this.y),
+	 		distanceX = Math.abs(newX - this.startX),
+	 		distanceY = Math.abs(newY - this.startY);
+	 		//超出边缘
+	 		if(newY > 0 || newY < this.maxScrollY) {
+	 			this.scrollTo(0,newY>0?0:this.maxScrollY,300,{style:'cubic-bezier(0.25, 0.46, 0.45, 0.94)'});
+	 		}
 	 		this.disable();
+	 	},
+	 	_transitionTime : function(time) {
+	 		this.scrollerStyle.transitionDuration = time + 'ms';
 	 	},
 	 };
 	 window.IScroll = IScroll;
